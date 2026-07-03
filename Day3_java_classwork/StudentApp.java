323cad87e1ec8b090af652a3c5813ea0aa44a2b7
Harshika-18 <davalaharshikadayal@gmail.com>
2026-07-03 20:40:19 +0530

    Day 3 java cw

---
+++ Day3_java_classwork/StudentApp.java
@@ -0,0 +1,11 @@
+package com.coforge.day3;
+
+public class StudentApp {
+
+	public static void main(String[] args) {
+		Student harshika = new Student(103, "Harshika", "h@exp.com");
+
+	    System.out.println(harshika);
+	}
+
+}
